6c4ee243e8fdf5eb34b845791ee6c4a61176a630
Otavio2007 <otavioaredes62@gmail.com>
2025-05-01 16:07:26 -0300

    Update Sistema_Bancário.py

--- Sistema_Bancário.py
+++ Sistema_Bancário.py
@@ -1,24 +1,22 @@
 import time
 
-opcao = 0
-dinheiro = 0
-extrato = []
-saques_realizados = 0  # Contador de saques no dia
-LIMITE_SAQUES = 3
-LIMITE_SAQUE_VALOR = 500.00
+# Senha inicial para acessar o sistema bancário
+SENHA_CORRETA = "python123"
 
-while opcao != 4:
-    opcao = int(input('\nO que deseja fazer?'
-                      '\n1 - Fazer depósito'
-                      '\n2 - Sacar dinheiro'
-                      '\n3 - Ver extrato'
-                      '\n4 - Encerrar operação -----> '))
+# Função que inicia o sistema bancário
+def sistema_bancario():
+    opcao = 0
+    dinheiro = 0
+    extrato = []
 
-    if opcao == 1:
-        senha = input('\nInsira sua senha: ')
-        print('Processando...')
-        time.sleep(2)
-        if senha == "python123":
+    while opcao != 4:
+        opcao = int(input('\nO que deseja fazer?'
+                          '\n1 - Fazer depósito'
+                          '\n2 - Sacar dinheiro'
+                          '\n3 - Ver extrato'
+                          '\n4 - Encerrar operação -----> '))
+
+        if opcao == 1:
             ad = float(input('\nInsira o valor do depósito: '))
             if ad > 0:
                 dinheiro += ad
@@ -26,37 +24,19 @@ while opcao != 4:
                 print('Depósito realizado com sucesso ;)')
             else:
                 print('Valor inválido! O depósito deve ser positivo.')
-        else:
-            print('\nSenha incorreta :(')
 
-    elif opcao == 2:
-        senha = input('\nInsira sua senha: ')
-        print('Processando...')
-        time.sleep(2)
-        if senha == "python123":
-            if saques_realizados >= LIMITE_SAQUES:
-                print('Limite diário de saques atingido!')
+        elif opcao == 2:
+            ret = float(input('\nValor a sacar: '))
+            if ret > dinheiro:
+                print('Saldo insuficiente para realizar o saque.')
+            elif ret <= 0:
+                print('Valor inválido! O saque deve ser positivo.')
             else:
-                ret = float(input('\nValor a sacar: '))
-                if ret > LIMITE_SAQUE_VALOR:
-                    print(f'O valor máximo por saque é de R$ {LIMITE_SAQUE_VALOR:.2f}')
-                elif ret > dinheiro:
-                    print('Saldo insuficiente para realizar o saque.')
-                elif ret <= 0:
-                    print('Valor inválido! O saque deve ser positivo.')
-                else:
-                    dinheiro -= ret
-                    extrato.append(f'Saque: -R$ {ret:.2f}')
-                    saques_realizados += 1
-                    print('Saque realizado com sucesso!')
-        else:
-            print('Senha incorreta :(')
+                dinheiro -= ret
+                extrato.append(f'Saque: -R$ {ret:.2f}')
+                print('Saque realizado com sucesso!')
 
-    elif opcao == 3:
-        senha = input('\nInsira sua senha: ')
-        print('Processando...')
-        time.sleep(2)
-        if senha == "python123":
+        elif opcao == 3:
             print('\n--- Extrato ---')
             if not extrato:
                 print('Nenhuma movimentação registrada.')
@@ -64,11 +44,20 @@ while opcao != 4:
                 for movimento in extrato:
                     print(movimento)
             print(f'\nSaldo atual: R$ {dinheiro:.2f}')
+
+        elif opcao == 4:
+            print('\nTenha um ótimo dia :)')
+
         else:
-            print('Senha incorreta :(')
+            print('\nOpção inválida, selecione as opções presentes na tela')
 
-    elif opcao == 4:
-        print('\nTenha um ótimo dia :)')
-
+# Função para autenticação inicial
+def autenticar():
+    senha_inicial = input("Insira a senha para iniciar o sistema bancário: ")
+    if senha_inicial == SENHA_CORRETA:
+        sistema_bancario()
     else:
-        print('\nOpção inválida, selecione as opções presentes na tela')
+        print("Senha incorreta. Acesso negado.")
+
+# Chama a função de autenticação antes de iniciar o sistema bancário
+autenticar()
