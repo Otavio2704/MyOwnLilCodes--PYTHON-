a68d53431fdf778933d91e20f6d3f741b4af82ab
Otavio2007 <otavioaredes62@gmail.com>
2025-05-13 17:14:06 -0300

    Update DoacaoDeSangue.py
    Separei em funções python

--- DoacaoDeSangue.py
+++ DoacaoDeSangue.py
@@ -1,42 +1,65 @@
-print('\nEsta é uma pesquisa para analisar se você é uma pessoa apta para realizar doação de sangue:')
+def verificar_idade():
+    idade = int(input('\nQual sua idade? '))
+    if 16 <= idade <= 69:
+        print('Sua idade é adequada para a doação')
+    elif idade < 16:
+        print('Você é menor de idade para realizar a doação')
+    else:
+        print('Sua idade é avançada para a doação')
+    return idade
 
-idade = int(input('\nQual sua idade? '))
-if idade >= 16 and idade <= 69:
-    print('Sua idade é adequada para a doação')
-elif idade < 16:
-    print('Você menor de idade para realizar a doação')
-else:
-    print('Sua idade é avançada para a doação')
+def verificar_peso():
+    peso = float(input("\nInsira seu peso: "))
+    if peso >= 50:
+        print('Você possui um peso adequado para a doação')
+    else:
+        print('Seu peso é inadequado para a doação')
+    return peso
 
-peso = float(input("\nInsira seu peso: "))
-if peso >= 50:
-    print('Você possui um peso adequado para a doação')
-else:
-    print('Seu peso é inadequado para a doação')
+def verificar_descanso():
+    desc = input("\nVocê dormiu pelo menos 5 horas nas últimas 24 horas? ").lower()
+    if desc == "sim":
+        print('Ótimo, você possui horas adequadas de descanso para a doação')
+        return True
+    else:
+        print('Necessita mais horas de descanso para a doação')
+        return False
 
+def verificar_alimentacao():
+    alim = input('\nEvitou alimentação gordurosa nas 4 horas que antecedem a doação? ').lower()
+    if alim == "sim":
+        print('Ótimo, você possui ótimos hábitos alimentares para a doação')
+        return True
+    else:
+        print('Necessita de uma alimentação mais adequada para a doação')
+        return False
 
-desc = input("\nVocê dormiu pelo menos 5 horas nas últimas 24 horas? ").lower()
-if desc == "sim":
-    print('ótimo, você possui horas adequadas de descanso para a doação')
-else:
-    print('Necessita mais horas de descanso para a doação')
+def verificar_doencas():
+    doenca = input ("\nPossui Hepatites B e C, AIDS (vírus HIV), doenças associadas aos vírus HTLV I e II ou Doença de Chagas? ").lower()
+    if doenca == "sim":
+        print('Então não poderá realizar a doação')
+        return True
+    else:
+        print('Ótimo, está saudável')
+        return False
 
-alim = input('\nEvitou alimentação gordurosa nas 4 horas que antecedem a doação? ').lower()
+def exibir_instrucoes_finais():
+    print('\n1- Caso você tenha entre 16 e 17 anos, peça autorização para seu responsável; '
+          'caso tenha entre 18 e 69 anos, poderá realizar a doação')
+    print('2- Caso tenha menos de 50 Kg, não poderá realizar a doação; senão, poderá realizar a doação normalmente')
+    print('3- Se você dormiu menos de 5 horas nas últimas 24 horas, não poderá realizar a doação')
+    print('4- Se caso tenha uma alimentação inadequada antes da doação, não poderá efetuar ela')
+    print('5- E caso possua alguma doença citada na pesquisa, também não poderá doar sangue')
+    print('\nTenha um ótimo dia, uma ótima tarde e uma ótima noite :)')
 
-if alim == "sim":
-    print('ótimo, você possui ótimos hábitos alimentares para a doação')
-else:
-    print('Necessita de uma alimentação mais adequada para a doação')
+def pesquisa_doacao_sangue():
+    print('\nEsta é uma pesquisa para analisar se você é uma pessoa apta para realizar doação de sangue:')
+    idade = verificar_idade()
+    peso = verificar_peso()
+    descanso = verificar_descanso()
+    alimentacao = verificar_alimentacao()
+    doencas = verificar_doencas()
+    exibir_instrucoes_finais()
 
-doenca = input ("\nPossui Hepatites B e C, AIDS (vírus HIV), doenças associadas aos vírus HTLV I e II ou Doença de Chagas? ").lower()
-if doenca == "sim":
-    print('Então não poderá realizar a doação')
-else:
-    print('Ótimo, está saudável')
-
-print('\n1- Caso você tenha entre 16 e 17 anos, peça autorização para seu responsável; caso tenha entre 18 e 69 anos, poderá realizar a doação')
-print('2- Caso tenha menos de 50 Kg, não poderá realizar a doação; senão, poderá realizar a doação normalmente')
-print('3- Se você dormiu menos de 5 horas nas últimas 24 horas, não poderá realizar a doação')
-print('4- Se caso tenha uma alimentação inadequada antes da doação, não poderá efetuar ela')
-print('5- E caso possui alguam doença citada na pesquisa, também não poderá doar sangue')
-print('\nTenha um ótimo dia, uma ótima tarde e uma ótima anoite :)')
+# Executar a pesquisa
+pesquisa_doacao_sangue()
