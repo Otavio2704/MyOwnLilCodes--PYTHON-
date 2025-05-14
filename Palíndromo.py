fb69a044632337ee1609d9fe7edf540178d02417
Otavio2007 <otavioaredes62@gmail.com>
2025-05-14 17:22:44 -0300

    Create Palíndromo.py

---
+++ Palíndromo.py
@@ -0,0 +1,14 @@
+#Analisar se é palíndromo ou não
+def verificar_palindromo(palavra):
+    palavra = palavra.upper().strip()
+    palavra_invertida = palavra[::-1]
+    
+    if palavra == palavra_invertida:
+        return 'Esta palavra é um palíndromo'
+    else:
+        return 'Esta palavra não é um palíndromo'
+
+#Adicionar palavra
+entrada = input('Insira uma palavra: ')
+resultado = verificar_palindromo(entrada)
+print(resultado)
