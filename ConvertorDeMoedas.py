bf2294c7a061254fd5fa66febc66e4d715487ed4
Otavio2007 <otavioaredes62@gmail.com>
2025-05-14 18:46:46 -0300

    Update ConvertorDeMoedas.py
    Atualizei a cotação de hoje (14/05/2025)

--- ConvertorDeMoedas.py
+++ ConvertorDeMoedas.py
@@ -1,6 +1,6 @@
 import unicodedata
 
-# Taxas de câmbio simuladas
+# Taxas de conversão
 taxas = {
     "BRL": {"USD": 0.18, "EUR": 0.16, "GBP": 0.13, "JPY": 26.00},
     "USD": {"BRL": 5.64, "EUR": 0.89, "GBP": 0.75, "JPY": 146.70},
@@ -9,38 +9,40 @@ taxas = {
     "JPY": {"BRL": 0.038, "USD": 0.0068, "EUR": 0.0061, "GBP": 0.0051}
 }
 
-# Mapeamento de nomes para códigos
+# Mapeamento de nomes na hora de escrever o valor
 nomes = {
     "real": "BRL", "dolar": "USD", "dólar": "USD", "euro": "EUR",
     "libra": "GBP", "iene": "JPY", "yen": "JPY", "ienes": "JPY", "yenes": "JPY"
 }
-
+#Remover o acento na hora de escrever "Dólar"
 def remover_acentos(txt):
     return ''.join(c for c in unicodedata.normalize('NFD', txt) if unicodedata.category(c) != 'Mn').lower()
-
-def pegar_codigo(nome):
+#Função que pega o número
+def pegar_moeda(nome):
     return nomes.get(remover_acentos(nome))
 
+# Mecanismo de conversão
 def converter(valor, de, para):
     if de == para:
         return valor
     return valor * taxas.get(de, {}).get(para, 0)
 
+
 def main():
     print("Moedas: REAL, DÓLAR, EURO, LIBRA, IENE")
-
-    de = pegar_codigo(input("De: "))
-    para = pegar_codigo(input("Para: "))
+#Inserir moedas
+    de = pegar_moeda(input("De: "))
+    para = pegar_moeda(input("Para: "))
     if not de or not para:
         print("Moeda inválida.")
         return
-
+#inserir valor
     try:
         valor = float(input("Valor: "))
     except ValueError:
         print("Valor inválido.")
         return
-
+#Resultado da conversão
     resultado = converter(valor, de, para)
     print(f"{valor:.2f} {de} = {resultado:.2f} {para}")
 
