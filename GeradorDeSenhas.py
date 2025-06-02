a0d0bf683e60b676831628fcd72ada767a0a5902
Otavio2007 <otavioaredes62@gmail.com>
2025-06-02 09:14:15 -0300

    Gerador de senhas com Python

--- GeradorDeSenhas.py
+++ GeradorDeSenhas.py
@@ -3,7 +3,7 @@ import string
 
 tamanho = 10  # tamanho da senha
 
-# Letras (maiúsculas e minúsculas) + números + símbolos
+# Letras (maiúsculas e minúsculas), números e símbolos
 caracteres = string.ascii_letters + string.digits + string.punctuation
 
 senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
